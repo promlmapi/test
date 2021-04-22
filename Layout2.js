08e71abf873883de4fa6988d4de2069bc09b8c3a
sathish <gncsathish@gmail.com>
2021-04-22 20:25:51 +0530

    IB-002 #Second commit

---
+++ Layout2.js
@@ -0,0 +1,511 @@
+export const EnTranslationLayout = {
+    "nav": {
+        "header": {
+            "links": {
+                "home": {
+                    "title": "Home"
+                },
+                "whyFpMarkets": {
+                    "title": "Why FP Markets"
+                },
+                "introducingBrokers": {
+                    "title": "Introducing Brokers"
+                },
+                "cpaAffiliates": {
+                    "title": "CPA Affiliates"
+                },
+                "marketingTools": {
+                    "title": "Marketing Tools"
+                },
+                "login": {
+                    "title": "Login"
+                },
+                "logout": {
+                    "title": "Logout"
+                },
+                "register": {
+                    "title": "Register"
+                },
+                "profile": {
+                    "title": "Profile"
+                },
+                "profileDetails": {
+                    "title": "Profile Details"
+                },
+                "contactDetails": {
+                    "title": "Contact Details"
+                },
+                "clients": {
+                    "title": "Clients"
+                },
+                "funding": {
+                    "title": "Funding"
+                },
+                "pending": {
+                    "title": "Pending"
+                },
+                "ibs": {
+                    "title": "IBs"
+                },
+                "groups": {
+                    "title": "Groups"
+                },
+                "transactions": {
+                    "title": "Transactions"
+                },
+                "actions": {
+                    "title": "Actions"
+                },
+                "createdat": {
+                    "title": "Created At"
+                },
+                "reports": {
+                    "title": "Reports"
+                },
+                "ibleads": {
+                    "title": "IB Leads"
+                },
+                "ibname": {
+                    "title": "IB Name"
+                },
+                "incompleteibregistrants": {
+                    "title": "Incomplete IB Registrants"
+                },
+                "incompleteibregistrants1": {
+                    "title": "Incomplete IB registrations"
+                },
+                "fullname": {
+                    "title": "Full Name"
+                },
+                "convertedlead": {
+                    "title": "Converted Lead"
+                },
+                "registrationdate": {
+                    "title": "Registration Date"
+                },
+                "registrationstepscompleted": {
+                    "title": "Registration steps completed"
+                },
+                "changeaddress": {
+                    "title": "Change Address"
+                },
+                "updateaddress": {
+                    "title": "Update Address"
+                },
+                "address": {
+                    "title": "Address"
+                },
+                "changepassword": {
+                    "title": "Change Password"
+                },
+                "referrallinks": {
+                    "title": "Referral Links"
+                },
+                "referrallinkerror": {
+                    "title": "You don't have any rebate accounts yet to have referral links."
+                },              
+                "managedocuments": {
+                    "title": "Manage Documents"
+                },
+                "contractfpmarkets": {
+                    "title": "Your contract with FP Markets."
+                },
+                "uploadeddocuments": {
+                    "title": "Uploaded documents."
+                },
+                "uploaddocument": {
+                    "title": "Upload document."
+                },
+                "uploaddocumentlimit": {
+                    "title": "Document upload Limit"
+                },
+                "uploaddocumentmsg": {
+                    "title": "(Please note you can upload maximum 4 files at a time by clicking Browse button below)"
+                },            
+                "accountnumber": {
+                    "title": "Account Number"
+                },
+                "accountname": {
+                    "title": "Account Name"
+                },
+                "agentcode": {
+                    "title": "Agent Code"
+                },
+                "agentname": {
+                    "title": "Agent Name"
+                },
+                "platform": {
+                    "title": "Platform"
+                },
+                "country": {
+                    "title": "Country"
+                },
+                "accountopeningdate": {
+                    "title": "Account Opening Date"
+                },
+                "totaldeposits": {
+                    "title": "Total Deposits"
+                },
+                "rebatesearned": {
+                    "title": "Rebates Earned"
+                },
+                "numberofclients": {
+                    "title": "Number of Clients"
+                },
+                "commissionlevel": {
+                    "title": "Commission Level"
+                },
+                "clientlist": {
+                    "title": "Client List"
+                },
+                "clientstradingactivity": {
+                    "title": "Clients Trading Activity"
+                },
+                "netdeposits": {
+                    "title": "Net Deposits"
+                },
+                "rebate": {
+                    "title": "Rebate"
+                },
+                "rebateaccount": {
+                    "title": "Rebate Account"
+                },
+                "requestdate": {
+                    "title": "Request Date"
+                },
+                "referredrebateaccount": {
+                    "title": "Referred Rebate Account Code"
+                },
+                "referredrebateaccountuser": {
+                    "title": "Referred Rebate Account User"
+                },
+                "lotstraded": {
+                    "title": "Lots Traded"
+                },
+                "commission": {
+                    "title": "Commission"
+                },
+                "clientscashactivity": {
+                    "title": "Clients Cash Activity"
+                },
+                "cashactivity": {
+                    "title": "Cash Activity"
+                },
+                "myleads": {
+                    "title": "My leads"
+                },
+                "direct": {
+                    "title": "Direct"
+                },
+                "mydirectleads": {
+                    "title": "My direct leads"
+                },
+                "leadname": {
+                    "title": "Lead Name"
+                },
+                "email": {
+                    "title": "Email"
+                },
+                "clientstage": {
+                    "title": "Client Stage"
+                },
+                "applicationstatus": {
+                    "title": "Application Status"
+                },
+                "notes": {
+                    "title": "Notes"
+                },
+                "leadstatus": {
+                    "title": "Lead Status"
+                },
+                "creationdate": {
+                    "title": "Creation Date"
+                },
+                "howdidyouhear": {
+                    "title": "How Did You Hear?"
+                },
+                "leadgrade": {
+                    "title": "Lead Grade"
+                },
+                "prioritypicklist": {
+                    "title": "Priority Picklist"
+                },
+                "promocode": {
+                    "title": "Promo Code"
+                },
+                "phone": {
+                    "title": "Phone"
+                },
+                "torebateaccounts": {
+                    "title": "To rebate accounts"
+                },
+                "torebatemsg1": {
+                    "title": "You don't have enough rebate accounts. Two or more rebate accounts are required to transfer funds between them."
+                },
+                "torebatemsg2": {
+                    "title": "Selected rebate account doesn't have any transferrable rebate account."
+                },
+                "torebatemsg3": {
+                    "title": "Transfer funds to rebate accounts"
+                },
+                "torebatemsg4": {
+                    "title": "Transfer funds"
+                },
+                "transferrequest": {
+                    "title": "Transfer requests"
+                },
+                "totradingaccounts": {
+                    "title": "To trading accounts"
+                },
+                "totradingmsg1": {
+                    "title": "You haven't added any trading account yet."
+                },
+                "totradingmsg2": {
+                    "title": "Selected rebate account doesn't have any trading account linked."
+                },
+                "totradingmsg3": {
+                    "title": "Transfer funds to trading accounts"
+                },
+                "totradingmsg4": {
+                    "title": "Transfer funds"
+                },
+                "tradinggrouplist": {
+                    "title": "Trading Group List"
+                },
+                "withdrawfunds": {
+                    "title": "Withdraw funds"
+                },
+                "withdrawfundmsg1": {
+                    "title": "You don't have any verified nominated bank account."
+                },
+                "withdrawfundmsg2": {
+                    "title": "Click"
+                },
+                "withdrawfundmsg3": {
+                    "title": "here"
+                },
+                "withdrawfundmsg4": {
+                    "title": "to add one"
+                },
+                "fromdate": {
+                    "title": "From Date"
+                },
+                "todate": {
+                    "title": "To Date"
+                },
+                "forgotpassword": {
+                    "title": "Forgot password?"
+                },
+                "loginaccount": {
+                    "title": "Login to your account"
+                },
+                "newtous": {
+                    "title": "New to us"
+                },
+                "registerhere": {
+                    "title": "Register here"
+                },
+                "instead": {
+                    "title": "instead"
+                },
+                "myindirectleads": {
+                    "title": "My indirect leads"
+                },
+                "indirect": {
+                    "title": "Indirect"
+                },
+                "transfer": {
+                    "title": "Transfer"
+                },
+                "withdrawfunds": {
+                    "title": "Withdraw Funds"
+                },
+                "phonenumber": {
+                    "title": "Phone number"
+                },
+                "withdrawalrequests": {
+                    "title": "Withdrawal Requests"
+                },
+                "linktradingaccount": {
+                    "title": "Link Trading Account"
+                },
+                "addtradinggroup": {
+                    "title": "Add Trading Group"
+                },
+                "linktradingaccountnew": {
+                    "title": "Link new trading account"
+                },
+                "linktradingaccountmsg1": {
+                    "title": "All your rebate accounts are already linked with trading accounts."
+                },
+                "managenominatedbankaccount": {
+                    "title": "Manage Nominated Bank Account"
+                },
+                "pendinganddeclinedapplications": {
+                    "title": "Pending & Declined applications"
+                },
+                "username": {
+                    "title": "User Name"
+                },
+                "parentibcode": {
+                    "title": "Parent IB Code"
+                },
+                "userlevel": {
+                    "title": "User Level"
+                },
+                "status": {
+                    "title": "Status"
+                },
+                "salesstatus": {
+                    "title": "Sales Status"
+                },
+                "submissiondate": {
+                    "title": "Submission Date"
+                },
+                "fundshistory": {
+                    "title": "Fund History"
+                },
+                "city": {
+                    "title": "City"
+                },
+                "state": {
+                    "title": "State"
+                },
+                "pendingnominatedbankaccounts": {
+                    "title": "Pending nominated bank accounts"
+                },
+                "bankname": {
+                    "title": "Bank Name"
+                },
+                "ibfullname": {
+                    "title": "IB Full Name"
+                },
+                "level": {
+                    "title": "Level"
+                },
+                "entity": {
+                    "title": "Entity"
+                },
+                "accountcode": {
+                    "title": "Account Code"
+                },
+                "type": {
+                    "title": "Type"
+                },
+                "amount": {
+                    "title": "Amount"
+                },
+                "currency": {
+                    "title": "Currency"
+                },
+                "transactiondate": {
+                    "title": "Transaction Date"
+                },
+                "description": {
+                    "title": "Description"
+                },
+                "application": {
+                    "title": "Application"
+                },
+                "nominatedbankaccounts": {
+                    "title": "Nominated Bank Accounts"
+                },
+                "nominatedbankaccountmsg1": {
+                    "title": "Manage nominated bank account"
+                },
+                "nominatedbankaccountmsg2": {
+                    "title": "Nominated Bank Account"
+                },
+                "iblists": {
+                    "title": "IB Lists"
+                },
+                "iblist": {
+                    "title": "IB list"
+                },
+                "ibtradingclientlead": {
+                    "title": "IB trading client leads"
+                },
+                "searchrebateaccounts": {
+                    "title": "Search Rebate Accounts"
+                },
+                "searchtradingclients": {
+                    "title": "Search Trading Clients"
+                },
+                "rrttype": {
+                    "title": "RRT Type"
+                },
+                "creationdate": {
+                    "title": "Creation Date"
+                },
+                "list": {
+                    "title": "List"
+                },
+                "add": {
+                    "title": "Add"
+                },
+                "callus": {
+                    "title": "Call us"
+                },
+                "withdrawals": {
+                    "title": "Withdrawals"
+                },
+                "cashadjustment": {
+                    "title": "Cash Adjustment"
+                },
+                "passwordfieldrequirements": {
+                    "title": "Password field requirements"
+                },
+                "pwdconstraint1": {
+                    "title": "Must be between 8-20 characters"
+                },
+                "pwdconstraint2": {
+                    "title": "Have at least one lower case letter"
+                },
+                "pwdconstraint3": {
+                    "title": "Have at least one upper case letter"
+                },
+                "pwdconstraint4": {
+                    "title": "Have at least one number and one symbol (?@!#()<>&*%)"
+                },
+                "documenttype": {
+                    "title": "Document Type"
+                },
+                "view": {
+                    "title": "View"
+                },
+                "appliedfor": {
+                    "title": "Applied For"
+                },
+                "status": {
+                    "title": "Status"
+                },
+                "welcomeback": {
+                    "title": "Welcome back!"
+                },
+                "forgotpwdcontent": {
+                    "title": "Password Reset link sent to your registered Email ID, Please check your inbox."
+                },
+                "locale": {
+                    "en": {
+                        "title": "EN"
+                    },
+                    "es": {
+                        "title": "ES"
+                    },
+                    "zh": {
+                        "title": "ZH"
+                    },
+                    "it": {
+                        "title": "IT"
+                    },
+                    "fr": {
+                        "title": "FR"
+                    }
+                }
+            }
+        },
+        "footer": {}
+    }
+};
+
+export default EnTranslationLayout;
