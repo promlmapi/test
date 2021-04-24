df0fdd9f541a663b88e83d9a7642679917fecb7b
sathish <gncsathish@gmail.com>
2021-04-24 15:01:18 +0530

    newly added es lang

---
+++ resources/js/translations/Es/Common.js
@@ -0,0 +1,25 @@
+export const EnTranslationCommon = {
+    "agreement": {
+        "title": "By continuing you confirm that you are over 18 years and agree to the terms of FP Markets privacy policy.",
+    },
+    "delete": {
+        "title": "Delete",
+    },
+    "next": {
+        "title": "Next",
+    },
+    "pleaseSelect": {
+        "title": "Please select",
+    },
+    "skip": {
+        "title": "Skip",
+    },
+    "submit": {
+        "title": "Submit",
+    },
+    "update": {
+        "title": "Update",
+    },
+};
+
+export default EnTranslationCommon;
