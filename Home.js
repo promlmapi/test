190589af7374f664eb8bd7ef4638f54b865fcf24
Srinivasan Lakshmipathi <s.lakshmipathi@fpmarkets.com.au>
2021-10-13 13:19:19 +0530

    IBP-0002 #new changes added in the branch feature translate changes

--- Home.js
+++ Home.js
@@ -5,6 +5,18 @@ export const EnTranslationLayout = {
                 "home": {
                     "title": "Home"
                 },
+                "home1": {
+                    "title": "Home1"
+                },
+                "home2": {
+                    "title": "Home2"
+                },
+                "home3": {
+                    "title": "Home3"
+                },
+                "home4": {
+                    "title": "Home4"
+                },
             }
         }
     }
